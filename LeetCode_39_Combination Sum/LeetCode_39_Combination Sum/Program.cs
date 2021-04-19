77424365aa633dc0152b3850cfe52bd421bcea08
BigStar1994123 <BigStar1994123@gmail.com>
2021-04-20 00:07:34 +0800

    LeetCode_39

--- LeetCode_39_Combination Sum/LeetCode_39_Combination Sum/Program.cs
+++ LeetCode_39_Combination Sum/LeetCode_39_Combination Sum/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode_39_Combination_Sum
 {
@@ -11,8 +12,8 @@ namespace LeetCode_39_Combination_Sum
 
             var s = new Solution();
 
-            var candidates = new int[] { 2, 3, 5 };
-            var target = 8;
+            var candidates = new int[] { 2, 3, 6, 7 };
+            var target = 7;
 
             var list = s.CombinationSum(candidates, target);
         }
@@ -22,6 +23,53 @@ namespace LeetCode_39_Combination_Sum
     {
         public IList<IList<int>> CombinationSum(int[] candidates, int target)
         {
+            Array.Sort(candidates);
+            var result = new List<IList<int>>();
+            GetResult(result, new List<int>(), candidates, target, 0);
+            return result;
+
+            //var result = new List<IList<int>>();
+
+            //for (int i = 0; i < candidates.Length; i++)
+            //{
+            //    var resultItem = new List<int>();
+
+            //    var value = candidates[i];
+            //    var subtraction = target;
+            //    while (true)
+            //    {
+            //        resultItem.Add(value);
+            //        subtraction -= value;
+
+            //        if (subtraction == 0)
+            //        {
+            //            result.Add(resultItem);
+            //            break;
+            //        }
+            //        else
+            //        {
+            //            resultItem.Remove(value);
+            //            break;
+            //        }
+            //    }
+            //}
+        }
+
+        private void GetResult(List<IList<int>> result, List<int> cur, int[] candidates, int target, int start)
+        {
+            if (target > 0)
+            {
+                for (int i = start; i < candidates.Length && target >= candidates[i]; i++)
+                {
+                    cur.Add(candidates[i]);
+                    GetResult(result, cur, candidates, target - candidates[i], i);
+                    cur.RemoveAt(cur.Count - 1);
+                }
+            }
+            else if (target == 0)
+            {
+                result.Add(new List<int>(cur));
+            }
         }
     }
 }
