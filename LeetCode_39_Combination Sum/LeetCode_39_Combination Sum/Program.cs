671ec936dedb3a7c7136a3a2da093156c0c298ab
BigStar1994123 <BigStar1994123@gmail.com>
2021-04-19 14:19:34 +0800

    LeetCode_39_Temp

---
+++ LeetCode_39_Combination Sum/LeetCode_39_Combination Sum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_39_Combination_Sum
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var s = new Solution();
+
+            var candidates = new int[] { 2, 3, 5 };
+            var target = 8;
+
+            var list = s.CombinationSum(candidates, target);
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+        }
+    }
+}
