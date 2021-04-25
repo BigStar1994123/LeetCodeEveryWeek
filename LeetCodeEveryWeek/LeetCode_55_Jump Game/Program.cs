d99e997f83ace15fd2aba222db9df67a95720475
BigStar1994123 <BigStar1994123@gmail.com>
2021-04-25 16:58:07 +0800

    LeetCode_55

---
+++ LeetCodeEveryWeek/LeetCode_55_Jump Game/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LeetCode_55_Jump_Game
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var s = new Solution();
+            var nums = new int[] { 2, 3, 1, 1, 4 };
+
+            Console.WriteLine(s.CanJump(nums));
+        }
+    }
+
+    public class Solution
+    {
+        public bool CanJump(int[] nums)
+        {
+            var finalPosition = nums.Length - 1;
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (i + nums[i] >= finalPosition)
+                {
+                    finalPosition = i;
+                }
+            }
+
+            return finalPosition == 0;
+        }
+    }
+}
