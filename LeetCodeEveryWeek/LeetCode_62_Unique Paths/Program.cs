68fdd5b7bbac9fb5e45f3c9bfed9158e5a1a21d7
BigStar1994123 <BigStar1994123@gmail.com>
2021-05-03 21:43:15 +0800

    LeetCode_62 LeetCode_70

---
+++ LeetCodeEveryWeek/LeetCode_62_Unique Paths/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LeetCode_62_Unique_Paths
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var s = new Solution();
+
+            var m = 3;
+            var n = 2;
+
+            Console.WriteLine(s.UniquePaths(m, n));
+        }
+    }
+
+    public class Solution
+    {
+        public int UniquePaths(int m, int n)
+        {
+            // 1: Wrire by DP
+            int[,] map = new int[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                map[i, 0] = 1;
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                map[0, j] = 1;
+            }
+
+            for (int i = 1; i < m; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    map[i, j] = map[i - 1, j] + map[i, j - 1];
+                }
+            }
+
+            return map[m - 1, n - 1];
+
+            // 2: Write by Combination
+        }
+    }
+}
---
+++ LeetCodeEveryWeek/LeetCode_70_Climbing Stairs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LeetCode_70_Climbing_Stairs
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var s = new Solution();
+
+            var n = 45;
+
+            Console.WriteLine(s.ClimbStairs(n));
+        }
+    }
+
+    public class Solution
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n == 1)
+                return 1;
+            if (n == 2)
+                return 2;
+
+            int[] dp = new int[n + 1];
+            dp[1] = 1;
+            dp[2] = 2;
+            for (int i = 3; i <= n; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+
+            return dp[n];
+
+            //return ClimbStairs(n - 1) + ClimbStairs(n - 2);
+        }
+    }
+}
