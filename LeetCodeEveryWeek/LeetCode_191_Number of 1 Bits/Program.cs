86f52be16aed485558c4b759fbed9e6bd69e32fc
BigStar1994123 <BigStar1994123@gmail.com>
2021-05-25 22:37:52 +0800

    LeetCode_191 LeetCode_198 7th week finish

---
+++ LeetCodeEveryWeek/LeetCode_191_Number of 1 Bits/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode_191_Number_of_1_Bits
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            uint num = 11;
+            var s = new Solution();
+            Console.WriteLine(s.HammingWeight(num));
+        }
+
+        public class Solution
+        {
+            public int HammingWeight(uint n)
+            {
+                var count = 0;
+
+                while (n > 0)
+                {
+                    if ((n & 1) == 1)
+                    {
+                        count++;
+                    }
+
+                    n >>= 1;
+                }
+
+                return count;
+            }
+        }
+    }
+}
---
+++ LeetCodeEveryWeek/LeetCode_198_House Robber/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode_198_House_Robber
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var nums = new int[] { 1, 2, 3, 1, 5, 9 };
+
+            var s = new Solution();
+            Console.WriteLine(s.Rob(nums));
+        }
+
+        public class Solution
+        {
+            public int Rob(int[] nums)
+            {
+                int[] dp = Enumerable.Repeat(0, nums.Length).ToArray();
+
+                var max = int.MinValue;
+
+                for (var i = 0; i < nums.Length; i++)
+                {
+                    int n3Value = 0;
+                    if (i >= 3)
+                    {
+                        n3Value = dp[i - 3];
+                    }
+                    int n2Value = 0;
+                    if (i >= 2)
+                    {
+                        n2Value = dp[i - 2];
+                    }
+
+                    var valueMax = Math.Max(n3Value + nums[i], n2Value + nums[i]);
+                    dp[i] = valueMax;
+                    max = Math.Max(max, valueMax);
+                }
+
+                return max;
+            }
+        }
+    }
+}
