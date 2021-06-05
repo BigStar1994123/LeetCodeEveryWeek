957340f970a282b80f893340a23e644d3f923091
BigStar1994123 <BigStar1994123@gmail.com>
2021-06-05 16:35:42 +0800

    LeetCode_217 LeetCode_226

---
+++ LeetCodeEveryWeek/LeetCode_217_Contains Duplicate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_217_Contains_Duplicate
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var nums = new int[] { 1, 23, 3, 4, 5 };
+
+            var s = new Solution();
+            Console.WriteLine(s.ContainsDuplicate(nums));
+        }
+
+        public class Solution
+        {
+            public bool ContainsDuplicate(int[] nums)
+            {
+                var hash = new HashSet<int>();
+
+                foreach (var num in nums)
+                {
+                    if (hash.Contains(num))
+                    {
+                        return true;
+                    }
+
+                    hash.Add(num);
+                }
+
+                return false;
+            }
+        }
+    }
+}
---
+++ LeetCodeEveryWeek/LeetCode_226_Invert Binary Tree/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace LeetCode_226_Invert_Binary_Tree
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = new TreeNode(4);
+            var b = new TreeNode(2);
+            var c = new TreeNode(7);
+            a.left = b;
+            a.right = c;
+
+            var s = new Solution();
+            s.InvertTree(a);
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            public TreeNode InvertTree(TreeNode root)
+            {
+                if (root == null)
+                {
+                    return root;
+                }
+
+                var leftNode = InvertTree(root.left);
+                var rightNode = InvertTree(root.right);
+
+                var tempNode = leftNode;
+                root.left = rightNode;
+                root.right = tempNode;
+
+                return root;
+            }
+        }
+    }
+}
