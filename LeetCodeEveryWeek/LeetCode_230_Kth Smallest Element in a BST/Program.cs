065b2e7968ba354fbc7796fc0e9230e602e69520
BigStar1994123 <BigStar1994123@gmail.com>
2021-06-06 02:52:03 +0800

    LeetCode_230 LeetCode_235

---
+++ LeetCodeEveryWeek/LeetCode_230_Kth Smallest Element in a BST/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCode_230_Kth_Smallest_Element_in_a_BST
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //var a = new TreeNode(3);
+            //var b = new TreeNode(1);
+            //var c = new TreeNode(4);
+            //a.left = b;
+            //a.right = c;
+
+            //var d = new TreeNode(2);
+            //b.left = null;
+            //b.right = d;
+
+            var a = new TreeNode(1);
+            var b = new TreeNode(2);
+            a.right = b;
+
+            var s = new Solution();
+            Console.WriteLine(s.KthSmallest(a, 2));
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            private int count = 0;
+
+            public int KthSmallest(TreeNode root, int k)
+            {
+                return Go(root, k).ResultValue;
+            }
+
+            public (bool IsGetVal, int ResultValue) Go(TreeNode node, int k)
+            {
+                var stack = new Stack<TreeNode>();
+
+                while (node != null)
+                {
+                    stack.Push(node);
+                    node = node.left;
+                }
+
+                while (stack.TryPop(out var popNode))
+                {
+                    count++;
+                    if (count == k)
+                    {
+                        return (true, popNode.val);
+                    }
+
+                    var result = Go(popNode.right, k);
+                    if (result.IsGetVal)
+                    {
+                        return (true, result.ResultValue);
+                    }
+                }
+
+                return (false, 0);
+            }
+        }
+    }
+}
---
+++ LeetCodeEveryWeek/LeetCode_235_Lowest_Common_Ancestor_Binary Search Tree/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace LeetCode_235_Lowest_Common_Ancestor_Binary_Search_Tree
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = new TreeNode(6);
+            var b = new TreeNode(2);
+            var c = new TreeNode(8);
+            a.left = b;
+            a.right = c;
+
+            //var d = new TreeNode(2);
+            //b.left = null;
+            //b.right = d;
+
+            var s = new Solution();
+            var r = s.LowestCommonAncestor(a, b, c);
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+            {
+                // 如果從root開始判斷的話，就可以分成幾種情況：
+                // 1.root的値和p或q的值相等 => 直接回傳root(p或q就是L)
+                // 2.root的値在p和q的值的範圍之內 => 直接回傳root
+                // (即p.val < L.val < q.val或p.val > L.val > q.val)
+                // 3.p和q的値都小於root的值 => 往下找root.left的狀況遞迴
+                // 4.p和q的値都大於root的值 => 往下找root.right的狀況遞迴
+
+                if (root.val == p.val || root.val == q.val)
+                {
+                    return root;
+                }
+
+                if (root.val < p.val && root.val > q.val)
+                {
+                    return root;
+                }
+                else if (root.val > p.val && root.val < q.val)
+                {
+                    return root;
+                }
+                else if (root.val > p.val && root.val > q.val)
+                {
+                    return LowestCommonAncestor(root.left, p, q);
+                }
+                else
+                {
+                    return LowestCommonAncestor(root.right, p, q);
+                }
+            }
+        }
+    }
+}
