6968937870f195c550f90d0f57592feb35b7eb1b
BigStar1994123 <BigStar1994123@gmail.com>
2021-06-05 15:34:05 +0800

    Special: LeetCode_714

---
+++ LeetCodeEveryWeek/LeetCode_714_BestTimeBuySellStock with Transaction Fee/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace LeetCode_714_BestTimeBuySellStock_with_Transaction_Fee
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var prices = new int[] { 1, 3, 2, 8, 4, 9 };
+            var fee = 2;
+
+            var s = new Solution();
+            Console.WriteLine(s.MaxProfit(prices, fee));
+        }
+
+        public class Solution
+        {
+            public int MaxProfit(int[] prices, int fee)
+            {
+                if (prices.Length == 0)
+                {
+                    return 0;
+                }
+
+                var profit = 0;
+                var currentProfit = 0;
+                var minPrice = prices[0];
+                var maxPrice = prices[0];
+
+                for (var i = 1; i < prices.Length; i++)
+                {
+                    minPrice = Math.Min(minPrice, prices[i]);
+                    maxPrice = Math.Max(maxPrice, prices[i]);
+                    currentProfit = Math.Max(currentProfit, prices[i] - minPrice - fee);
+
+                    if (maxPrice - prices[i] >= fee)
+                    {
+                        profit += currentProfit;
+                        currentProfit = 0;
+                        minPrice = prices[i];
+                        maxPrice = prices[i];
+                    }
+                }
+
+                return profit + currentProfit;
+            }
+        }
+    }
+}
