b6d591fffc0f735de506f0f10da6484701114756
BigStar1994123 <BigStar1994123@gmail.com>
2021-06-22 18:45:31 +0800

    LeetCode_300 LeetCode_302 11th week finish

---
+++ LeetCodeEveryWeek/LeetCode_300_Longest Increasing Subsequence/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_300_Longest_Increasing_Subsequence
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //var nums = new int[] { 0, 1, 0, 3, 2, 3 };
+            //var nums = new int[] { 10, 9, 2, 5, 3, 7, 101, 18, 19 };
+            var nums = new int[] { 7, 7, 7, 7, 7, 7, 7 };
+
+            var s = new Solution();
+            Console.WriteLine(s.LengthOfLIS(nums));
+        }
+
+        public class Solution
+        {
+            public int LengthOfLIS(int[] nums)
+            {
+                if (nums.Length == 0)
+                {
+                    return 0;
+                }
+
+                var dpList = new List<int>();
+
+                foreach (var num in nums)
+                {
+                    for (var i = 0; ; i++)
+                    {
+                        if (i >= dpList.Count)
+                        {
+                            dpList.Add(num);
+                            break;
+                        }
+
+                        if (num <= dpList[i])
+                        {
+                            dpList[i] = num;
+                            break;
+                        }
+                    }
+                }
+
+                return dpList.Count;
+            }
+        }
+    }
+}
---
+++ LeetCodeEveryWeek/LeetCode_322_Coin Change/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode_322_Coin_Change
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var coins = new int[] { 5, 4 };
+            var amount = 11;
+
+            var s = new Solution();
+            Console.WriteLine(s.CoinChange(coins, amount));
+        }
+
+        public class Solution
+        {
+            public int CoinChange(int[] coins, int amount)
+            {
+                var dp = Enumerable.Repeat(amount + 1, amount + 1).ToArray();
+                dp[0] = 0;
+
+                for (int i = 1; i <= amount; i++)
+                {
+                    for (int j = 0; j < coins.Length; j++)
+                    {
+                        if (coins[j] <= i)
+                        {
+                            dp[i] = Math.Min(dp[i], dp[i - coins[j]] + 1);
+                        }
+                    }
+                }
+
+                return (dp[amount] > amount) ? -1 : dp[amount];
+            }
+        }
+    }
+}
