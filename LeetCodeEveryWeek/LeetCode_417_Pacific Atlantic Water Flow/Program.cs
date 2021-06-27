ccbde6676a0479dbff6402cc3cf5ee161ff63824
BigStar1994123 <BigStar1994123@gmail.com>
2021-06-27 19:22:37 +0800

    LeetCode_417_temp

---
+++ LeetCodeEveryWeek/LeetCode_417_Pacific Atlantic Water Flow/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_417_Pacific_Atlantic_Water_Flow
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var heights = new int[][] { };
+
+            var s = new Solution();
+            var list = s.PacificAtlantic(heights);
+        }
+
+        public class Solution
+        {
+            public IList<IList<int>> PacificAtlantic(int[][] heights)
+            {
+            }
+        }
+    }
+}
