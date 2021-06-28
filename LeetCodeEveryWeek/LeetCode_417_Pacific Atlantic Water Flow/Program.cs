1f3962d58871c9a53407f3ecdf126a304a40bcf7
BigStar1994123 <BigStar1994123@gmail.com>
2021-06-29 02:15:59 +0800

    LeetCode_417 LeetCode_424

--- LeetCodeEveryWeek/LeetCode_417_Pacific Atlantic Water Flow/Program.cs
+++ LeetCodeEveryWeek/LeetCode_417_Pacific Atlantic Water Flow/Program.cs
@@ -17,8 +17,68 @@ namespace LeetCode_417_Pacific_Atlantic_Water_Flow
 
         public class Solution
         {
-            public IList<IList<int>> PacificAtlantic(int[][] heights)
+            public IList<IList<int>> PacificAtlantic(int[][] matrix)
             {
+                var row = matrix.Length;
+
+                var result = new List<IList<int>>();
+                if (row == 0) return result;
+
+                var col = matrix[0].Length;
+
+                var pacific = new bool[row, col];
+                var atlantic = new bool[row, col];
+
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < col; j++)
+                    {
+                        if (i == 0 || j == 0)
+                        {
+                            DFS(matrix, int.MinValue, i, j, pacific);
+                        }
+
+                        if (i == row - 1 || j == col - 1)
+                        {
+                            DFS(matrix, int.MinValue, i, j, atlantic);
+                        }
+                    }
+                }
+
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < col; j++)
+                    {
+                        if (pacific[i, j] && atlantic[i, j])
+                        {
+                            result.Add(new List<int>() { i, j });
+                        }
+                    }
+                }
+
+                return result;
+            }
+
+            private void DFS(int[][] matrix, int pre, int x, int y, bool[,] isVisited)
+            {
+                var row = matrix.Length;
+                var col = matrix[0].Length;
+
+                if (x >= row || x < 0 || y >= col || y < 0) return;
+                if (isVisited[x, y]) return;
+
+                if (pre > matrix[x][y]) return;
+
+                isVisited[x, y] = true;
+
+                var directions = new (int, int)[] { (0, 1), (0, -1), (1, 0), (-1, 0) };
+                foreach (var direction in directions)
+                {
+                    var nextX = x + direction.Item1;
+                    var nextY = y + direction.Item2;
+
+                    DFS(matrix, matrix[x][y], nextX, nextY, isVisited);
+                }
             }
         }
     }
---
+++ LeetCodeEveryWeek/LeetCode_424_Longest Repeating Character Replacement/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace LeetCode_424_Longest_Repeating_Character_Replacement
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var s = new Solution();
+
+            var text = "ABCDAA";
+            var k = 2;
+
+            Console.WriteLine(s.CharacterReplacement(text, k));
+        }
+
+        public class Solution
+        {
+            public int CharacterReplacement(string s, int k)
+            {
+                var len = s.Length;
+                if (len < 2)
+                {
+                    return len;
+                }
+
+                var charArray = s.ToCharArray();
+                var freq = new int[26];
+                var maxCount = 0;
+
+                int left = 0, right = 0, result = 0;
+                while (right < len)
+                {
+                    freq[charArray[right] - 'A']++;
+                    maxCount = Math.Max(maxCount, freq[charArray[right] - 'A']);
+                    right++;
+
+                    if (right - left > maxCount + k)
+                    {
+                        freq[charArray[left] - 'A']--;
+                        left++;
+                    }
+
+                    result = Math.Max(result, right - left);
+                }
+
+                return result;
+            }
+        }
+    }
+}
