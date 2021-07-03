16e09c89c841a6029ab8d2f760a53862d39498b5
BigStar1994123 <BigStar1994123@gmail.com>
2021-07-03 15:19:06 +0800

    LeetCode_572 LeetCode_647

---
+++ LeetCodeEveryWeek/LeetCode_572_Subtree of Another Tree/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace LeetCode_572_Subtree_of_Another_Tree
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //var aTree = new TreeNode(3);
+            //aTree.left = new TreeNode(4, new TreeNode(1), new TreeNode(2, new TreeNode(1), null));
+            //aTree.right = new TreeNode(5);
+
+            //var bTree = new TreeNode(4, new TreeNode(1), new TreeNode(2));
+
+            var aTree = new TreeNode(1, new TreeNode(1), null);
+            var bTree = new TreeNode(1);
+
+            var s = new Solution();
+            Console.WriteLine(s.IsSubtree(aTree, bTree));
+        }
+
+        // Definition for a binary tree node.
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            public bool IsSubtree(TreeNode root, TreeNode subRoot)
+            {
+                if (root == null)
+                {
+                    return false;
+                }
+
+                if (IsSame(root, subRoot))
+                {
+                    return true;
+                }
+
+                return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
+            }
+
+            private bool IsSame(TreeNode a, TreeNode b)
+            {
+                if (a == null && b == null)
+                {
+                    return true;
+                }
+
+                if ((a != null && b == null) || (a == null && b != null) || a.val != b.val)
+                {
+                    return false;
+                }
+
+                return IsSame(a.left, b.left) && IsSame(a.right, b.right);
+            }
+        }
+    }
+}
---
+++ LeetCodeEveryWeek/LeetCode_647_Palindromic Substrings/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace LeetCode_647_Palindromic_Substrings
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var text = "aabaaca";
+
+            var s = new Solution();
+            Console.WriteLine(s.CountSubstrings(text));
+        }
+
+        public class Solution
+        {
+            public int CountSubstrings(string s)
+            {
+                int n = s.Length;
+
+                if (n == 1)
+                {
+                    return 1;
+                }
+
+                var dp = new int[n + 1][];
+
+                for (int i = 0; i < n + 1; i++)
+                {
+                    dp[i] = new int[n + 1];
+                }
+
+                dp[0][0] = 1; // "" is palindromic
+
+                for (int i = 1; i <= n; i++)
+                {
+                    dp[i][i] = 1;
+                }
+
+                var count = n;  // one char string is palindromic
+
+                for (int length = 2; length <= n; length++)
+                {
+                    for (int start = 0; start + length <= n; start++)
+                    {
+                        int end = length + start - 1;
+                        //                          two chars         || at least three chars
+                        if (s[start] == s[end] && ((start + 1 == end) || dp[start + 1][end - 1] == 1))
+                        {
+                            count++;
+                            dp[start][end] = 1;
+                        }
+                    }
+                }
+
+                return count;
+            }
+        }
+    }
+}
