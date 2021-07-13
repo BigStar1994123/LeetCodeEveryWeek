76be358bd2459a853a23b869f086414d867bc22f
BigStar1994123 <BigStar1994123@gmail.com>
2021-07-13 10:10:19 +0800

    LeetCode_142 LeetCode_160

---
+++ LeetCodeEveryWeek/LeetCode_142_Linked List Cycle II/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_142_Linked_List_Cycle_II
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = new ListNode(1);
+            var b = new ListNode(2);
+            var c = new ListNode(3);
+            var d = new ListNode(4);
+
+            a.next = b;
+            b.next = a;
+            //b.next = c;
+            //c.next = d;
+            //d.next = b;
+
+            var s = new Solution();
+            var result = s.DetectCycle(a);
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+
+        public class Solution
+        {
+            public ListNode DetectCycle(ListNode head)
+            {
+                var fastPointer = head;
+                var slowPointer = head;
+                ListNode meetPointer = null;
+
+                while (fastPointer != null && fastPointer.next != null)
+                {
+                    fastPointer = fastPointer.next.next;
+                    slowPointer = slowPointer.next;
+
+                    if (fastPointer == slowPointer)
+                    {
+                        meetPointer = slowPointer;
+                        break;
+                    }
+                }
+
+                slowPointer = head;
+                while (slowPointer != meetPointer)
+                {
+                    slowPointer = slowPointer.next;
+                    meetPointer = meetPointer.next;
+                }
+
+                return slowPointer;
+            }
+        }
+    }
+}
---
+++ LeetCodeEveryWeek/LeetCode_160_Intersection of Two Linked Lists/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_160_Intersection_of_Two_Linked_Lists
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = new ListNode(4);
+            var b = new ListNode(1);
+            a.next = b;
+
+            var c = new ListNode(8);
+            b.next = c;
+
+            var d = new ListNode(5);
+            var e = new ListNode(6);
+            var f = new ListNode(1);
+            d.next = e;
+            e.next = f;
+            f.next = c;
+
+            c.next = new ListNode(4);
+
+            var s = new Solution();
+            var result = s.GetIntersectionNode(a, d);
+        }
+
+        public class Solution
+        {
+            public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+            {
+                var dp = new HashSet<ListNode>();
+
+                while (headA != null)
+                {
+                    dp.Add(headA);
+                    headA = headA.next;
+                }
+
+                while (headB != null)
+                {
+                    if (dp.Contains(headB))
+                    {
+                        return headB;
+                    }
+
+                    headB = headB.next;
+                }
+
+                return null;
+            }
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int x)
+            {
+                val = x;
+            }
+        }
+    }
+}
