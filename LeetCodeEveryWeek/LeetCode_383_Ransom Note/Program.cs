f4aad4b2ff12510c8496b63204b3d8ada99acdc9
BigStar1994123 <BigStar1994123@gmail.com>
2021-07-13 17:48:41 +0800

    LeetCode_383

---
+++ LeetCodeEveryWeek/LeetCode_383_Ransom Note/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_383_Ransom_Note
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var rNote = "aabb";
+
+            var magazine = "aab";
+
+            var s = new Solution();
+            Console.WriteLine(s.CanConstruct(rNote, magazine));
+        }
+
+        public class Solution
+        {
+            public bool CanConstruct(string ransomNote, string magazine)
+            {
+                var magazineDp = new Dictionary<char, int>();
+
+                foreach (var mChar in magazine)
+                {
+                    magazineDp[mChar] = magazineDp.ContainsKey(mChar) ? (magazineDp[mChar] + 1) : 1;
+                }
+
+                foreach (var rChar in ransomNote)
+                {
+                    if (!magazineDp.ContainsKey(rChar) || magazineDp[rChar] < 1)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        magazineDp[rChar]--;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+}
