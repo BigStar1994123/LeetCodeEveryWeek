7e87dffd8b07fe02a86297d121ea62fd24fbfd96
BigStar1994123 <BigStar1994123@gmail.com>
2021-07-13 17:36:28 +0800

    LeetCode_242_ReWrite

--- LeetCodeEveryWeek/LeetCode_242_Valid Anagram/Program.cs
+++ LeetCodeEveryWeek/LeetCode_242_Valid Anagram/Program.cs
@@ -9,12 +9,63 @@ namespace LeetCode_242_Valid_Anagram
         {
             Console.WriteLine("Hello World!");
 
-            var s = new Solution();
+            var s = new Solution2();
 
             var a = "ab";
             var b = "a";
 
             Console.WriteLine(s.IsAnagram(a, b));
+        }
+
+        public class Solution2
+        {
+            public bool IsAnagram(string s, string t)
+            {
+                var dp = new Dictionary<char, int>();
+                var dp2 = new Dictionary<char, int>();
+
+                foreach (var sChar in s)
+                {
+                    if (!dp.ContainsKey(sChar))
+                    {
+                        dp.Add(sChar, 1);
+                    }
+                    else
+                    {
+                        dp[sChar]++;
+                    }
+                }
+
+                foreach (var tChar in t)
+                {
+                    if (!dp2.ContainsKey(tChar))
+                    {
+                        dp2.Add(tChar, 1);
+                    }
+                    else
+                    {
+                        dp2[tChar]++;
+                    }
+                }
+
+                foreach (var keyValuePair in dp)
+                {
+                    if (!dp2.ContainsKey(keyValuePair.Key) || dp2[keyValuePair.Key] != keyValuePair.Value)
+                    {
+                        return false;
+                    }
+                }
+
+                foreach (var keyValuePair in dp2)
+                {
+                    if (!dp.ContainsKey(keyValuePair.Key) || dp[keyValuePair.Key] != keyValuePair.Value)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
         }
 
         public class Solution
