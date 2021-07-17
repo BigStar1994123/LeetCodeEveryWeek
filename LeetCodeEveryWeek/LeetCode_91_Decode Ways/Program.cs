a1510dd4a2bf9b320fc42c4de74074b98436e49e
BigStar1994123 <BigStar1994123@gmail.com>
2021-07-17 16:53:01 +0800

    LeetCode_91_Rewrite

--- LeetCodeEveryWeek/LeetCode_91_Decode Ways/Program.cs
+++ LeetCodeEveryWeek/LeetCode_91_Decode Ways/Program.cs
@@ -9,11 +9,77 @@ namespace LeetCode_91_Decode_Ways
         {
             Console.WriteLine("Hello World!");
 
-            var s = new Solution();
+            var s = new Solution2();
 
-            var text = "111111111111111111111111111111111111111111111";
+            //var text = "111111111111111111111111111111111111111111111";
+            var text = "10011";
+            //var text = "226";
 
             Console.WriteLine(s.NumDecodings(text));
+        }
+    }
+
+    public class Solution2
+    {
+        public int NumDecodings(string s)
+        {
+            var dp = new Dictionary<int, int>();
+
+            if (s.Length == 0)
+            {
+                return 0;
+            }
+
+            if (s.Length == 1)
+            {
+                if (s[0] == '0')
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            else if (s[0] == '0')
+            {
+                return 0;
+            }
+
+            dp.Add(-1, 1);
+            dp.Add(0, 1);
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                var num = Convert.ToInt32(s.Substring(i - 1, 2));
+
+                if (s[i] == '0' && (num > 26 || num == 0))
+                {
+                    return 0;
+                }
+
+                if (num < 27)
+                {
+                    if (s[i - 1] == '0')
+                    {
+                        dp[i] = dp[i - 1];
+                    }
+                    else if (s[i] == '0')
+                    {
+                        dp[i] = dp[i - 2];
+                    }
+                    else
+                    {
+                        dp[i] = dp[i - 1] + dp[i - 2];
+                    }
+                }
+                else
+                {
+                    dp[i] = dp[i - 1];
+                }
+            }
+
+            return dp[s.Length - 1];
         }
     }
 
