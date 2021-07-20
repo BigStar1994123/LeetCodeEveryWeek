d010da8ad5b76cb8240090a09bf83921ab4d1bd4
BigStar1994123 <BigStar1994123@gmail.com>
2021-07-20 20:23:54 +0800

    LeetCode_151 LeetCode_344 15th week finish

---
+++ LeetCodeEveryWeek/LeetCode_151_Reverse Words in a String/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode_151_Reverse_Words_in_a_String
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var text = "the sky is blue";
+            var s = new Solution();
+
+            Console.WriteLine(s.ReverseWords(text));
+        }
+
+        public class Solution
+        {
+            public string ReverseWords(string s)
+            {
+                var list = new List<string>();
+
+                var text = string.Empty;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] != ' ')
+                    {
+                        text += s[i];
+                    }
+                    else
+                    {
+                        if (!string.IsNullOrEmpty(text))
+                        {
+                            list.Add(text);
+                            text = string.Empty;
+                        }
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(text))
+                {
+                    list.Add(text);
+                }
+
+                list.Reverse();
+
+                return string.Join(" ", list);
+
+                //var splits = s.Split(" ");
+
+                //return string.Join(" ", splits.Where(x => !string.IsNullOrEmpty(x)).Reverse());
+            }
+        }
+    }
+}
---
+++ LeetCodeEveryWeek/LeetCode_344_Reverse String/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LeetCode_344_Reverse_String
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var chars = new char[] { 'h', 'e', 'l', 'l', 'o' };
+
+            var s = new Solution();
+            s.ReverseString(chars);
+        }
+
+        public class Solution
+        {
+            public void ReverseString(char[] s)
+            {
+                var last = s.Length - 1;
+                var i = 0;
+                char temp;
+
+                while (i < s.Length / 2)
+                {
+                    temp = s[i];
+                    s[i] = s[last - i];
+                    s[last - i] = temp;
+                    i++;
+                }
+            }
+        }
+    }
+}
