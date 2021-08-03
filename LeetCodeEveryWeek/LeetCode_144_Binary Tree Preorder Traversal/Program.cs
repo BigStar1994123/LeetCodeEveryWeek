ac1d30023017a562aaff3b67d5009132669c2d31
BigStar1994123 <BigStar1994123@gmail.com>
2021-08-03 23:45:32 +0800

    LeetCode_144 LeetCode_347

---
+++ LeetCodeEveryWeek/LeetCode_144_Binary Tree Preorder Traversal/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_144_Binary_Tree_Preorder_Traversal
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null));
+
+            var s = new Solution();
+            var result = s.PreorderTraversal(a);
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            public IList<int> PreorderTraversal(TreeNode root)
+            {
+                var result = new List<int>();
+
+                Exec(root, result);
+
+                return result;
+            }
+
+            private void Exec(TreeNode node, List<int> result)
+            {
+                if (node == null)
+                {
+                    return;
+                }
+
+                result.Add(node.val);
+                Exec(node.left, result);
+                Exec(node.right, result);
+            }
+        }
+    }
+}
--- LeetCodeEveryWeek/LeetCode_347_Top K Frequent Elements/Program.cs
+++ LeetCodeEveryWeek/LeetCode_347_Top K Frequent Elements/Program.cs
@@ -13,8 +13,34 @@ namespace LeetCode_347_Top_K_Frequent_Elements
             var nums = new int[] { 1, 1, 1, 2, 2, 3 };
             var k = 2;
 
-            var s = new Solution();
+            var s = new Solution2();
             Console.WriteLine(string.Join(",", s.TopKFrequent(nums, k)));
+        }
+
+        public class Solution2
+        {
+            public int[] TopKFrequent(int[] nums, int k)
+            {
+                var numToFreq = new Dictionary<int, int>();
+                foreach (var n in nums)
+                    numToFreq[n] = numToFreq.GetValueOrDefault(n, 0) + 1;
+                //return numToFreq.OrderByDescending(x => x.Value).Take(k).Select(x => x.Key).ToArray();
+
+                var heapDict = new SortedDictionary<int, object>(Comparer<int>.Create((x, y) =>
+                {
+                    var result = numToFreq[y].CompareTo(numToFreq[x]);
+                    return result != 0 ? result : x.CompareTo(y);
+                }));
+
+                foreach (var i in numToFreq.Keys)
+                {
+                    heapDict[i] = null;
+                    if (heapDict.Count > k)
+                        heapDict.Remove(heapDict.Last().Key);
+                }
+
+                return heapDict.Keys.ToArray();
+            }
         }
 
         public class Solution
