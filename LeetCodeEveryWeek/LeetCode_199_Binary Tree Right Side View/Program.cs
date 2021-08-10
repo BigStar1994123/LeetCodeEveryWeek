97cc1db6501a3ac96713f68faceea91e32684045
BigStar1994123 <BigStar1994123@gmail.com>
2021-08-10 23:08:17 +0800

    LeetCode_199 LeetCode_637 18th week finish

---
+++ LeetCodeEveryWeek/LeetCode_199_Binary Tree Right Side View/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_199_Binary_Tree_Right_Side_View
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = new TreeNode(3);
+            var b = new TreeNode(9);
+            var c = new TreeNode(20);
+            var d = new TreeNode(15);
+            var e = new TreeNode(7);
+
+            a.left = b;
+            a.right = c;
+            c.left = d;
+            c.right = e;
+
+            var s = new Solution();
+            var list = s.RightSideView(a);
+            Console.WriteLine(string.Join(",", list));
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            public IList<int> RightSideView(TreeNode root)
+            {
+                if (root == null)
+                {
+                    return new List<int>();
+                }
+
+                var result = new List<int>();
+                var queue = new Queue<TreeNode>();
+                queue.Enqueue(root);
+
+                while (queue.Count > 0)
+                {
+                    var queueSize = queue.Count;
+                    for (int i = 0; i < queueSize; i++)
+                    {
+                        var node = queue.Dequeue();
+
+                        if (node.left != null)
+                            queue.Enqueue(node.left);
+
+                        if (node.right != null)
+                            queue.Enqueue(node.right);
+
+                        if (i == queueSize - 1)
+                            result.Add(node.val);
+                    }
+                }
+
+                return result;
+            }
+        }
+    }
+}
---
+++ LeetCodeEveryWeek/LeetCode_637_Average of Levels in Binary Tree/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_637_Average_of_Levels_in_Binary_Tree
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = new TreeNode(3);
+            var b = new TreeNode(9);
+            var c = new TreeNode(20);
+            var d = new TreeNode(15);
+            var e = new TreeNode(7);
+
+            a.left = b;
+            a.right = c;
+            c.left = d;
+            c.right = e;
+
+            var s = new Solution();
+            var list = s.AverageOfLevels(a);
+            Console.WriteLine(string.Join(",", list));
+        }
+
+        public class Solution
+        {
+            public IList<double> AverageOfLevels(TreeNode root)
+            {
+                if (root == null)
+                    return new List<double>();
+
+                var result = new List<double>();
+                var queue = new Queue<TreeNode>();
+                queue.Enqueue(root);
+
+                while (queue.Count > 0)
+                {
+                    var queueSize = queue.Count;
+
+                    var sum = 0d;
+                    for (int i = 0; i < queueSize; i++)
+                    {
+                        var node = queue.Dequeue();
+
+                        if (node.left != null)
+                            queue.Enqueue(node.left);
+
+                        if (node.right != null)
+                            queue.Enqueue(node.right);
+
+                        sum += node.val;
+                    }
+
+                    result.Add(sum / queueSize);
+                }
+
+                return result;
+            }
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+    }
+}
