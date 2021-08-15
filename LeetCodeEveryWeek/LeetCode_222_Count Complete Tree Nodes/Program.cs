6530c21d2f29cf49ba84b5c188696d0dbb464b12
BigStar1994123 <BigStar1994123@gmail.com>
2021-08-15 21:21:40 +0800

    LeetCode_222_Temp

---
+++ LeetCodeEveryWeek/LeetCode_222_Count Complete Tree Nodes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LeetCode_222_Count_Complete_Tree_Nodes
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var head = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3, new TreeNode(6)));
+
+            var s = new Solution();
+            Console.WriteLine(s.CountNodes(head));
+        }
+
+        // Definition for a binary tree node.
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            public int CountNodes(TreeNode root)
+            {
+            }
+        }
+    }
+}
