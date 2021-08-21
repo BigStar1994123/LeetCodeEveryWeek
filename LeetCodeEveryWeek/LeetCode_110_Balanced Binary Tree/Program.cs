f494af9ecb73a26b420279a6b71f0a7686f87461
BigStar1994123 <BigStar1994123@gmail.com>
2021-08-21 15:53:36 +0800

    LeetCode_110 LeetCode_257

---
+++ LeetCodeEveryWeek/LeetCode_110_Balanced Binary Tree/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace LeetCode_110_Balanced_Binary_Tree
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var node = new TreeNode(3);
+            var leftNode = new TreeNode(9);
+            node.left = leftNode;
+
+            var rightNode = new TreeNode(20, new TreeNode(15), new TreeNode(7));
+            node.right = rightNode;
+
+            var s = new Solution();
+            Console.WriteLine(s.IsBalanced(node));
+        }
+
+        // Definition for a binary tree node.
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            public bool IsBalanced(TreeNode root)
+            {
+                var result = GetHigh(root);
+                return result.balanced;
+            }
+
+            private (int high, bool balanced) GetHigh(TreeNode node)
+            {
+                if (node == null)
+                {
+                    return (0, true);
+                }
+
+                var highLeft = GetHigh(node.left);
+                if (!highLeft.balanced)
+                    return (0, false);
+
+                var highRight = GetHigh(node.right);
+                if (!highRight.balanced)
+                    return (0, false);
+
+                if (Math.Abs(highLeft.high - highRight.high) > 1)
+                {
+                    return (0, false);
+                }
+
+                var highMax = Math.Max(highLeft.high, highRight.high);
+                return (1 + highMax, true);
+            }
+        }
+    }
+}
---
+++ LeetCodeEveryWeek/LeetCode_257_Binary Tree Paths/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_257_Binary_Tree_Paths
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = new TreeNode(1);
+            var leftNode = new TreeNode(2, null, new TreeNode(5));
+            a.left = leftNode;
+
+            var rightNode = new TreeNode(3);
+            a.right = rightNode;
+
+            var s = new Solution();
+            var result = s.BinaryTreePaths(a);
+        }
+
+        // Definition for a binary tree node.
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            public IList<string> BinaryTreePaths(TreeNode root)
+            {
+                var result = new List<string>();
+
+                Go(root, result, new List<int>());
+
+                return result;
+            }
+
+            private void Go(TreeNode node, IList<string> result, IList<int> nodeHistory)
+            {
+                if (node == null)
+                {
+                    return;
+                }
+
+                nodeHistory.Add(node.val);
+
+                if (node.left == null && node.right == null)
+                {
+                    result.Add(string.Join("->", nodeHistory));
+                    nodeHistory.RemoveAt(nodeHistory.Count - 1);
+                    return;
+                }
+
+                Go(node.left, result, nodeHistory);
+                Go(node.right, result, nodeHistory);
+
+                nodeHistory.RemoveAt(nodeHistory.Count - 1);
+            }
+        }
+    }
+}
