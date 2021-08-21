e0d14695bb8b0f0b0258771e31011ebd5debf609
BigStar1994123 <BigStar1994123@gmail.com>
2021-08-21 12:53:14 +0800

    LeetCode_222_another version

--- LeetCodeEveryWeek/LeetCode_222_Count Complete Tree Nodes/Program.cs
+++ LeetCodeEveryWeek/LeetCode_222_Count Complete Tree Nodes/Program.cs
@@ -8,9 +8,11 @@ namespace LeetCode_222_Count_Complete_Tree_Nodes
         {
             Console.WriteLine("Hello World!");
 
-            var head = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3, new TreeNode(6)));
+            var head = new TreeNode(1,
+                new TreeNode(5,
+     new TreeNode(4), new TreeNode(2)), new TreeNode(7, new TreeNode(6)));
 
-            var s = new Solution();
+            var s = new Solution2();
             Console.WriteLine(s.CountNodes(head));
         }
 
@@ -39,6 +41,53 @@ namespace LeetCode_222_Count_Complete_Tree_Nodes
                 }
 
                 return 1 + CountNodes(root.left) + CountNodes(root.right);
+            }
+        }
+
+        /// <summary>
+        /// Eddie's Version
+        /// </summary>
+        public class Solution2
+        {
+            public int CountNodes(TreeNode root)
+            {
+                if (root == null)
+                    return 0;
+                var depth = GetDepth(root);
+                var lo = (int)Math.Pow(2, depth - 1);
+                var hi = (int)Math.Pow(2, depth) - 1;
+
+                while (hi != lo)
+                {
+                    var val = (lo + hi) / 2 + 1;
+                    var node = GetNodeByVal(root, val);
+                    if (node == null)
+                        hi = val - 1;
+                    else
+                        lo = val;
+                }
+                return hi;
+            }
+
+            public int GetDepth(TreeNode root)
+            {
+                if (root == null)
+                    return 0;
+                return 1 + GetDepth(root.left);
+            }
+
+            public TreeNode GetNodeByVal(TreeNode root, int val)
+            {
+                var binaryString = Convert.ToString(val, 2);
+                TreeNode result = root;
+                for (int i = 1; i < binaryString.Length; i++)
+                {
+                    if (binaryString[i] == '0')
+                        result = result.left;
+                    else
+                        result = result.right;
+                }
+                return result;
             }
         }
     }
