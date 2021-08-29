32ef6962b56fd14feedf973c86801303ff20d7d4
BigStar1994123 <BigStar1994123@gmail.com>
2021-08-30 01:58:41 +0800

    LeetCode_617 LeetCode_700

---
+++ LeetCodeEveryWeek/LeetCode_617_Merge Two Binary Trees/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace LeetCode_617_Merge_Two_Binary_Trees
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var aTree = new TreeNode(1, new TreeNode(3, new TreeNode(5)), new TreeNode(2));
+            var bTree = new TreeNode(2, new TreeNode(1, null, new TreeNode(4)), new TreeNode(3, null, new TreeNode(7)));
+
+            var s = new Solution();
+            var result = s.MergeTrees(aTree, bTree);
+        }
+
+        // Definition for a binary tree node.
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+            {
+                if (root1 != null && root2 != null)
+                {
+                    var result = new TreeNode(((root1?.val) ?? 0) + ((root2?.val) ?? 0))
+                    {
+                        left = MergeTrees(root1.left, root2.left),
+                        right = MergeTrees(root1.right, root2.right)
+                    };
+                    return result;
+                }
+                else if (root1 != null && root2 == null)
+                {
+                    return root1;
+                }
+                else if (root1 == null && root2 != null)
+                {
+                    return root2;
+                }
+
+                return null;
+            }
+        }
+    }
+}
---
+++ LeetCodeEveryWeek/LeetCode_700_Search in a Binary Search Tree/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LeetCode_700_Search_in_a_Binary_Search_Tree
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var node = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7));
+            var val = 2;
+
+            var s = new Solution();
+            var result = s.SearchBST(node, val);
+        }
+
+        // Definition for a binary tree node.
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            public TreeNode SearchBST(TreeNode root, int val)
+            {
+                if (root == null || root.val == val)
+                {
+                    return root;
+                }
+
+                var leftResult = SearchBST(root.left, val);
+                var rightResult = SearchBST(root.right, val);
+
+                return leftResult ?? rightResult;
+            }
+        }
+    }
+}
