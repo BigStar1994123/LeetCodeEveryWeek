4e2f1f345e441fb4cb6bf090d035c390ab135769
BigStar1994123 <BigStar1994123@gmail.com>
2021-09-05 21:39:24 +0800

    LeetCode_501 LeetCode_530

---
+++ src/LeetCode_501_Find Mode in Binary Search Tree/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode_501_Find_Mode_in_Binary_Search_Tree
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var root = new TreeNode(1, null, new TreeNode(2, new TreeNode(2)));
+
+            var s = new Solution();
+            var result = s.FindMode(root);
+        }
+
+        // Definition for a binary tree node.
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            public int[] FindMode(TreeNode root)
+            {
+                Dictionary<int, int> map = new Dictionary<int, int>();
+                var result = new List<int>();
+
+                DFS(root, map);
+
+                var ordered = map.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value).ToList();
+                result.Add(ordered[0].Key);
+
+                for (int i = 1; i < ordered.Count; i++)
+                {
+                    if (ordered[i].Value == ordered[0].Value)
+                    {
+                        result.Add(ordered[i].Key);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                return result.ToArray();
+            }
+
+            private void DFS(TreeNode node, Dictionary<int, int> map)
+            {
+                if (node == null)
+                {
+                    return;
+                }
+
+                if (!map.ContainsKey(node.val))
+                {
+                    map[node.val] = 1;
+                }
+                else
+                {
+                    map[node.val]++;
+                }
+
+                DFS(node.left, map);
+                DFS(node.right, map);
+                return;
+            }
+        }
+    }
+}
---
+++ src/LeetCode_530_Minimum Absolute Difference in BST/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace LeetCode_530_Minimum_Absolute_Difference_in_BST
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // [236,104,701,null,227,null,911]
+
+            //var node = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(6));
+            var node = new TreeNode(236, new TreeNode(104, null, new TreeNode(227)), new TreeNode(701, null, new TreeNode(911)));
+
+            var s = new Solution();
+            Console.WriteLine(s.GetMinimumDifference(node));
+        }
+
+        // Definition for a binary tree node.
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            private TreeNode preNode;
+            private int result = int.MaxValue;
+
+            public int GetMinimumDifference(TreeNode root)
+            {
+                if (root == null)
+                {
+                    return 0;
+                }
+
+                Traversal(root);
+                return result;
+            }
+
+            private void Traversal(TreeNode node)
+            {
+                if (node == null)
+                {
+                    return;
+                }
+
+                // Left
+                Traversal(node.left);
+
+                // Mid
+                if (preNode != null)
+                {
+                    result = Math.Min(result, node.val - preNode.val);
+                }
+
+                preNode = node;
+
+                // Right
+                Traversal(node.right);
+            }
+        }
+    }
+}
