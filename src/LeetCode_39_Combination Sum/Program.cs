00599f438d2ab224e38e276a874649cbb7b35466
BigStar1994123 <BigStar1994123@gmail.com>
2021-09-14 00:23:29 +0800

    LeetCode_39_re-write 23th week finish

--- src/LeetCode_39_Combination Sum/Program.cs
+++ src/LeetCode_39_Combination Sum/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode_39_Combination_Sum
 {
@@ -9,12 +10,52 @@ namespace LeetCode_39_Combination_Sum
         {
             Console.WriteLine("Hello World!");
 
-            var s = new Solution();
+            var s = new Solution2();
 
             var candidates = new int[] { 2, 3, 6, 7 };
             var target = 7;
 
-            var list = s.CombinationSum(candidates, target);
+            var result = s.CombinationSum(candidates, target);
+            Console.WriteLine(string.Join("\n", result.Select(x => string.Join("|", x))));
+        }
+    }
+
+    public class Solution2
+    {
+        private IList<IList<int>> _result = new List<IList<int>>();
+        private IList<int> _path = new List<int>();
+
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            Array.Sort(candidates);
+            BackTracking(candidates, target, 0, 0);
+            return _result;
+        }
+
+        private void BackTracking(int[] candidates, int target, int startIndex, int sum)
+        {
+            if (sum == target)
+            {
+                _result.Add(_path.ToList());
+                return;
+            }
+
+            if (sum > target)
+            {
+                return;
+            }
+
+            for (int i = startIndex; i < candidates.Length; i++)
+            {
+                if (sum + candidates[i] > target)
+                {
+                    break;
+                }
+
+                _path.Add(candidates[i]);
+                BackTracking(candidates, target, i, sum + candidates[i]);
+                _path.RemoveAt(_path.Count - 1);
+            }
         }
     }
 
