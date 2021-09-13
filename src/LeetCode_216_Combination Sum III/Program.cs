c41c885aaaa354758bbe882d9dd2fae807b078e2
BigStar1994123 <BigStar1994123@gmail.com>
2021-09-13 23:28:53 +0800

    LeetCode_216 LeetCode_77

---
+++ src/LeetCode_216_Combination Sum III/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode_216_Combination_Sum_III
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var k = 9;
+            var sum = 45;
+
+            var s = new Solution();
+            var result = s.CombinationSum3(k, sum);
+            Console.WriteLine(string.Join("\n", result.Select(x => string.Join("|", x))));
+        }
+
+        public class Solution
+        {
+            private IList<IList<int>> _result = new List<IList<int>>();
+            private IList<int> _path = new List<int>();
+
+            public IList<IList<int>> CombinationSum3(int k, int n)
+            {
+                BackTracking(k, n, 1, 0);
+                return _result;
+            }
+
+            private void BackTracking(int k, int n, int startIndex, int tmpSum)
+            {
+                if (_path.Count > k)
+                {
+                    return;
+                }
+
+                if (tmpSum == n && _path.Count == k)
+                {
+                    _result.Add(_path.ToList());
+                    return;
+                }
+
+                for (int i = startIndex; i <= n - tmpSum && i <= 9; i++)
+                {
+                    _path.Add(i);
+                    BackTracking(k, n, i + 1, tmpSum + i);
+                    _path.RemoveAt(_path.Count - 1);
+                }
+            }
+        }
+    }
+}
---
+++ src/LeetCode_77_Combinations/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode_77_Combinations
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var n = 4;
+            var k = 2;
+
+            var s = new Solution();
+            var result = s.Combine(n, k);
+            Console.WriteLine(string.Join("\n", result.Select(x => string.Join("|", x))));
+        }
+
+        public class Solution
+        {
+            private List<IList<int>> _result = new List<IList<int>>();
+            private List<int> _path = new List<int>();
+
+            public IList<IList<int>> Combine(int n, int k)
+            {
+                BackTracking(n, k, 1);
+                return _result;
+            }
+
+            private void BackTracking(int n, int k, int startIndex)
+            {
+                if (_path.Count == k)
+                {
+                    _result.Add(_path.ToList());
+                    return;
+                }
+
+                for (int i = startIndex; i <= n; i++)
+                {
+                    _path.Add(i);
+                    BackTracking(n, k, i + 1);
+                    _path.RemoveAt(_path.Count - 1);
+                }
+            }
+        }
+    }
+}
