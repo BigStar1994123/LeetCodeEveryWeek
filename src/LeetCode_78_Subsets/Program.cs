730b8358947f9af901a4289cc2fdda13e2c20061
BigStar1994123 <BigStar1994123@gmail.com>
2021-09-20 01:43:53 +0800

    LeetCode_78 LeetCode_90

---
+++ src/LeetCode_78_Subsets/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode_78_Subsets
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var nums = new int[] { 1, 2, 3 };
+
+            var s = new Solution();
+            var result = s.Subsets(nums);
+
+            Console.WriteLine(string.Join("\n", result.Select(x => string.Join("|", x))));
+        }
+
+        public class Solution
+        {
+            private IList<IList<int>> _result = new List<IList<int>>();
+            private IList<int> _path = new List<int>();
+
+            public IList<IList<int>> Subsets(int[] nums)
+            {
+                BackTracking(nums, 0);
+                return _result;
+            }
+
+            private void BackTracking(int[] nums, int startIndex = 0)
+            {
+                _result.Add(_path.ToList());
+
+                if (startIndex == nums.Length)
+                {
+                    return;
+                }
+
+                for (int i = startIndex; i < nums.Length; i++)
+                {
+                    _path.Add(nums[i]);
+                    BackTracking(nums, i + 1);
+                    _path.RemoveAt(_path.Count - 1);
+                }
+            }
+        }
+    }
+}
---
+++ src/LeetCode_90_Subsets II/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode_90_Subsets_II
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var nums = new int[] { 1, 2, 2, 2 };
+
+            var s = new Solution();
+            var result = s.SubsetsWithDup(nums);
+
+            Console.WriteLine(string.Join("\n", result.Select(x => string.Join(",", x))));
+        }
+
+        public class Solution
+        {
+            private IList<IList<int>> _result = new List<IList<int>>();
+            private IList<int> _path = new List<int>();
+
+            public IList<IList<int>> SubsetsWithDup(int[] nums)
+            {
+                Array.Sort(nums);
+                BackTracking(nums, 0);
+                return _result;
+            }
+
+            private void BackTracking(int[] nums, int startIndex)
+            {
+                _result.Add(_path.ToList());
+
+                if (startIndex == nums.Length)
+                {
+                    return;
+                }
+
+                var hash = new HashSet<int>();
+                for (int i = startIndex; i < nums.Length; i++)
+                {
+                    if (hash.Contains(nums[i]))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        hash.Add(nums[i]);
+                    }
+
+                    _path.Add(nums[i]);
+                    BackTracking(nums, i + 1);
+                    _path.RemoveAt(_path.Count - 1);
+                }
+            }
+        }
+    }
+}
