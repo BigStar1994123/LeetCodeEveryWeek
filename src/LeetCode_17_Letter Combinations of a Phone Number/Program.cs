ff715e98056e3e00bcedfb54a96814e16857020e
BigStar1994123 <BigStar1994123@gmail.com>
2021-09-20 00:23:15 +0800

    LeetCode_17 LeetCode_40

---
+++ src/LeetCode_17_Letter Combinations of a Phone Number/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode_17_Letter_Combinations_of_a_Phone_Number
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var digits = "23";
+
+            var s = new Solution();
+            var result = s.LetterCombinations(digits);
+        }
+
+        public class Solution
+        {
+            private Dictionary<char, char[]> _digitMap = new Dictionary<char, char[]>()
+            {
+                { '2', new char[] { 'a', 'b', 'c' } },
+                { '3', new char[] { 'd', 'e', 'f'} },
+                { '4', new char[] { 'g', 'h', 'i' } },
+                { '5', new char[] { 'j', 'k', 'l' } },
+                { '6', new char[] { 'm', 'n', 'o' } },
+                { '7', new char[] { 'p', 'q', 'r', 's'} },
+                { '8', new char[] { 't', 'u', 'v' } },
+                { '9', new char[] { 'w', 'x', 'y', 'z'} }
+            };
+
+            private IList<string> _result = new List<string>();
+            private IList<char> _path = new List<char>();
+
+            public IList<string> LetterCombinations(string digits)
+            {
+                if (digits.Length == 0)
+                {
+                    return _result;
+                }
+
+                BackTracking(digits, 0);
+                return _result;
+            }
+
+            private void BackTracking(string digits, int startIndex)
+            {
+                if (startIndex == digits.Length)
+                {
+                    _result.Add(new string(_path.ToArray()));
+                    return;
+                }
+
+                var words = _digitMap[digits[startIndex]];
+
+                for (int j = 0; j < words.Length; j++)
+                {
+                    _path.Add(words[j]);
+                    BackTracking(digits, startIndex + 1);
+                    _path.RemoveAt(_path.Count - 1);
+                }
+            }
+        }
+    }
+}
---
+++ src/LeetCode_40_Combination Sum II/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode_40_Combination_Sum_II
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var candidates = new int[] { 10, 1, 2, 7, 6, 1, 5 };
+            //var candidates = new int[] { 1, 1, 2, 4 };
+            var target = 8;
+
+            var s = new Solution();
+            var result = s.CombinationSum2(candidates, target);
+        }
+
+        public class Solution
+        {
+            private IList<IList<int>> _result = new List<IList<int>>();
+            private IList<int> _path = new List<int>();
+
+            public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+            {
+                Array.Sort(candidates);
+                BackTracking(candidates, target, 0, 0);
+                _result.Distinct();
+                return _result;
+            }
+
+            private void BackTracking(int[] candidates, int target, int startIndex, int sum)
+            {
+                if (sum == target)
+                {
+                    _result.Add(_path.ToList());
+                    return;
+                }
+
+                if (sum > target)
+                {
+                    return;
+                }
+
+                var hash = new HashSet<int>();
+                for (int i = startIndex; i < candidates.Length; i++)
+                {
+                    if (hash.Contains(candidates[i]))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        hash.Add(candidates[i]);
+                    }
+
+                    _path.Add(candidates[i]);
+                    BackTracking(candidates, target, i + 1, sum + candidates[i]);
+                    _path.RemoveAt(_path.Count - 1);
+                }
+            }
+        }
+    }
+}
