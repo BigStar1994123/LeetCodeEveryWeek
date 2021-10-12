48de3ce47e1b3fcb2c15c39069fdbd764199a73a
BigStar1994123 <BigStar1994123@gmail.com>
2021-10-13 01:01:36 +0800

    LeetCode_135 LeetCode_452 27th week finish

---
+++ src/LeetCode_135_Candy/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode_135_Candy
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var rating = new int[] { 1, 0, 2, 3, 4, 4, 3, 2, 1 };
+
+            var s = new Solution();
+            Console.WriteLine(s.Candy(rating));
+        }
+
+        public class Solution
+        {
+            public int Candy(int[] ratings)
+            {
+                var candies = Enumerable.Repeat(1, ratings.Length).ToList();
+
+                candies[0] = 1;
+                candies[ratings.Length - 1] = 1;
+
+                // 由左向右走
+                for (int i = 1; i < ratings.Length; i++)
+                {
+                    if (ratings[i] > ratings[i - 1])
+                    {
+                        candies[i] = candies[i - 1] + 1;
+                    }
+                }
+
+                // 由右向左走
+                for (int i = ratings.Length - 2; i >= 0; i--)
+                {
+                    if (ratings[i] > ratings[i + 1])
+                    {
+                        candies[i] = Math.Max(candies[i], candies[i + 1] + 1);
+                    }
+                }
+
+                // 統計結果
+                var result = 0;
+                for (int i = 0; i < candies.Count; i++)
+                {
+                    result += candies[i];
+                }
+
+                return result;
+            }
+        }
+    }
+}
---
+++ src/LeetCode_452_Minimum Number of Arrows to Burst Balloons/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode_452_Minimum_Number_of_Arrows_to_Burst_Balloons
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var points = new int[][]
+            {
+                //new int[] { 10, 16 },
+                //new int[] { 2, 8 },
+                //new int[] { 1, 6 },
+                //new int[] { 7, 12 }
+                new int[] { 1, 2 },
+                new int[] { 2, 3 },
+                new int[] { 3, 4 },
+                new int[] { 4, 5 }
+            };
+
+            var s = new Solution();
+            Console.WriteLine(s.FindMinArrowShots(points));
+        }
+
+        public class Solution
+        {
+            public int FindMinArrowShots(int[][] points)
+            {
+                var sortedPoints = points.ToList().OrderBy(x => x[1]);
+
+                var count = 0;
+                int? nowPos = null;
+
+                foreach (var p in sortedPoints)
+                {
+                    if (nowPos == null || p[0] > nowPos.Value)
+                    {
+                        count++;
+                        nowPos = p[1];
+                    }
+                }
+
+                return count;
+            }
+        }
+    }
+}
