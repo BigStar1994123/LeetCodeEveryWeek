963ee45b810d22bc137a6794fffbfc4a7efe83aa
BigStar1994123 <BigStar1994123@gmail.com>
2021-10-31 14:47:52 +0800

    LeetCode_343 LeetCode_63

---
+++ src/LeetCode_343_Integer Break/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode_343_Integer_Break
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var num = 12;
+
+            var s = new Solution2();
+            Console.WriteLine(s.IntegerBreak(num));
+        }
+
+        public class Solution2
+        {
+            public int IntegerBreak(int n)
+            {
+                if (n == 2)
+                {
+                    return 1;
+                }
+                else if (n == 3)
+                {
+                    return 2;
+                }
+
+                var dp = new Dictionary<int, int>();
+                dp.Add(1, 1);
+                dp.Add(2, 2);
+                dp.Add(3, 3);
+                // 4, 4
+                // 5, 6
+                // 6, 9
+
+                for (int i = 4; i <= n; i++)
+                {
+                    var val = Math.Max(dp[i - 2] * 2, dp[i - 3] * 3);
+                    dp.Add(i, val);
+                }
+
+                return (dp[n]);
+            }
+        }
+
+        /// <summary>
+        /// Wrong
+        /// </summary>
+        public class Solution
+        {
+            private IList<int> _path = new List<int>();
+
+            private Dictionary<string, int> _productDp = new Dictionary<string, int>
+            {
+                { "", 1 }
+            };
+
+            private int _maxProduct;
+
+            private int _num;
+
+            public int IntegerBreak(int n)
+            {
+                _num = n;
+                BackTracking(n);
+                return _maxProduct;
+            }
+
+            private void BackTracking(int n)
+            {
+                if (n < 1)
+                {
+                    return;
+                }
+
+                for (int i = (int)Math.Floor(Math.Sqrt(_num)); i <= (_num / 2) + 1; i++)
+                {
+                    var pathString = string.Join(".", _path);
+                    var dpValue = string.IsNullOrWhiteSpace(pathString) ? 1 : _productDp[pathString];
+
+                    _path.Add(i);
+
+                    var product = dpValue * i;
+                    _productDp.Add(string.Join(".", _path), product);
+
+                    if (_path.Count >= 2)
+                    {
+                        _maxProduct = Math.Max(_maxProduct, product);
+                    }
+
+                    BackTracking(n - i);
+                    _path.RemoveAt(_path.Count - 1);
+                }
+            }
+        }
+    }
+}
---
+++ src/LeetCode_63_Unique Paths II/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace LeetCode_63_Unique_Paths_II
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var grid = new int[][]
+            {
+                //new int[] { 0,0,0 },
+                //new int[] { 0,1,0 },
+                //new int[] { 0,0,0 }
+
+                new int[] { 1,0 },
+                new int[] { 0,0 }
+            };
+
+            var s = new Solution();
+            Console.WriteLine(s.UniquePathsWithObstacles(grid));
+        }
+
+        public class Solution
+        {
+            public int UniquePathsWithObstacles(int[][] obstacleGrid)
+            {
+                var m = obstacleGrid.Length;
+                var n = obstacleGrid[0].Length;
+
+                var dp = new int[m][];
+
+                var flag = false;
+                for (int i = 0; i < dp.Length; i++)
+                {
+                    dp[i] = new int[n];
+                    if (!flag && obstacleGrid[i][0] == 1)
+                    {
+                        flag = true;
+                    }
+
+                    dp[i][0] = flag ? 0 : 1;
+                }
+
+                var flag2 = false;
+                for (int i = 0; i < dp[0].Length; i++)
+                {
+                    if (!flag2 && obstacleGrid[0][i] == 1)
+                    {
+                        flag2 = true;
+                    }
+
+                    dp[0][i] = flag2 ? 0 : 1;
+                }
+
+                for (int i = 1; i < dp.Length; i++)
+                {
+                    for (int j = 1; j < dp[0].Length; j++)
+                    {
+                        dp[i][j] = obstacleGrid[i][j] == 1 ? 0 : dp[i - 1][j] + dp[i][j - 1];
+                    }
+                }
+
+                return dp[^1][dp[0].Length - 1];
+            }
+        }
+    }
+}
