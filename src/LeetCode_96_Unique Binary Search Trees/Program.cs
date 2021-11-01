953032e4b5fcfa7067dbe6534c16fe413742daa4
BigStar1994123 <BigStar1994123@gmail.com>
2021-11-02 00:31:21 +0800

    LeetCode_96

---
+++ src/LeetCode_96_Unique Binary Search Trees/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode_96_Unique_Binary_Search_Trees
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var num = 4;
+
+            var s = new Solution();
+            Console.WriteLine(s.NumTrees(num));
+        }
+
+        public class Solution
+        {
+            public int NumTrees(int n)
+            {
+                var dp = new Dictionary<int, int>()
+                {
+                    { 0, 1 },
+                    { 1, 1 }
+                };
+
+                for (int i = 2; i <= n; i++)
+                {
+                    var value = 0;
+                    var subNodeCounts = i - 1;
+                    for (int j = 0; j < i; j++)
+                    {
+                        value += dp[j] * dp[subNodeCounts - j];
+                    }
+
+                    dp.Add(i, value);
+                }
+
+                return dp[n];
+            }
+        }
+    }
+}
