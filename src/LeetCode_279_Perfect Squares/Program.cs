189fe7a471858f68efe7ba131a860298efa902e8
BigStar1994123 <BigStar1994123@gmail.com>
2021-11-16 01:41:55 +0800

    LeetCode_279

---
+++ src/LeetCode_279_Perfect Squares/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode_279_Perfect_Squares
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var n = 16;
+
+            var s = new Solution();
+            Console.WriteLine(s.NumSquares(n));
+        }
+
+        public class Solution
+        {
+            public int NumSquares(int n)
+            {
+                var dp = Enumerable.Repeat(int.MaxValue, n + 1).ToArray();
+                dp[0] = 0;
+
+                var sq = Math.Sqrt(n);
+
+                for (int i = 1; i <= sq; i++)
+                {
+                    var squareValue = (int)Math.Pow(i, 2);
+
+                    for (int j = squareValue; j <= n; j++)
+                    {
+                        if (dp[j - squareValue] != int.MaxValue)
+                        {
+                            dp[j] = Math.Min(dp[j - squareValue] + 1, dp[j]);
+                        }
+                    }
+                }
+
+                return dp[n];
+            }
+        }
+    }
+}
