dc508105b0b5363da143cf09d7225376d60ba8d4
BigStar1994123 <BigStar1994123@gmail.com>
2022-01-10 21:27:39 +0800

    LeetCode_1035 LeetCode_1143

---
+++ src/LeetCode_1035_Uncrossed Lines/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var s = new Solution();
+var nums1 = new int[] { 1, 3, 7, 1, 7, 5 };
+var nums2 = new int[] { 1, 9, 2, 5, 1 };
+
+Console.WriteLine(s.MaxUncrossedLines(nums1, nums2));
+
+public class Solution
+{
+    public int MaxUncrossedLines(int[] nums1, int[] nums2)
+    {
+        var dp = Enumerable.Repeat(0, nums1.Length + 1).Select(x => Enumerable.Repeat(0, nums2.Length + 1).ToArray()).ToArray();
+
+        for (int i = 1; i <= nums1.Length; i++)
+        {
+            for (int j = 1; j <= nums2.Length; j++)
+            {
+                if (nums1[i - 1] == nums2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    dp[i][j] = Math.Max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[nums1.Length][nums2.Length];
+    }
+}
---
+++ src/LeetCode_1143_Longest Common Subsequence/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var s = new Solution();
+var text1 = "bl";
+var text2 = "yby";
+
+Console.WriteLine(s.LongestCommonSubsequence(text1, text2));
+
+public class Solution
+{
+    public int LongestCommonSubsequence(string text1, string text2)
+    {
+        //Enumerable.Range(0, nums1.Length + 1).Select(x => Enumerable.Repeat(0, nums2.Length + 1).ToArray()).ToArray();
+        var dp = Enumerable.Range(0, text1.Length + 1).Select(x => Enumerable.Repeat<int>(0, text2.Length + 1).ToArray()).ToArray();
+
+        for (int i = 1; i <= text1.Length; i++)
+        {
+            for (int j = 1; j <= text2.Length; j++)
+            {
+                if (text1[i - 1] == text2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    dp[i][j] = Math.Max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[text1.Length][text2.Length];
+    }
+}
