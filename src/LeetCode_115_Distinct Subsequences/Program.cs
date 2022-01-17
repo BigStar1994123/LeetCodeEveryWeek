1d2ba37eac954ec86258f5ad3590d1584351a321
BigStar1994123 <BigStar1994123@gmail.com>
2022-01-17 14:46:08 +0800

    LeetCode_115 LeetCode_392

---
+++ src/LeetCode_115_Distinct Subsequences/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var s = new Solution();
+
+var text1 = "rabbbit";
+var text2 = "rabbit";
+
+Console.WriteLine(s.NumDistinct(text1, text2));
+
+public class Solution
+{
+    public int NumDistinct(string s, string t)
+    {
+    }
+}
---
+++ src/LeetCode_392_Is Subsequence/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var s = new Solution();
+Console.WriteLine(s.IsSubsequence("abc", "ahbgd"));
+
+public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        var i = 0;
+
+        for (int j = 0; j < t.Length; j++)
+        {
+            if (i == s.Length)
+            {
+                return true;
+            }
+
+            if (s[i] == t[j])
+            {
+                i++;
+            }
+        }
+
+        return i == s.Length;
+    }
+}
